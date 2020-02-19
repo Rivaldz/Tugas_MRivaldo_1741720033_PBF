4c0fdd27a9622eb2bf1813d361d3d38cd6678f42
Rivaldz <rivalzsetyo@gmail.com>
2020-02-19 12:20:40 +0700

    membuat navbar,footer biodata

--- mrivaldo-minggu3/src/App.js
+++ mrivaldo-minggu3/src/App.js
@@ -5,20 +5,123 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav class="navbar fixed-top  navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark"> 
+         <a class="navbar-brand" href="#">Navbar</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
         </a>
-      </header>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="card kartu">
+    <div class="row">
+      <div class="col-md4">
+        <div class="foto">
+           <img src="https://malangvoice.com/wp-content/uploads/2019/06/polinema.jpg" alt="" width="300" height="auto"/>
+        </div>   
+      </div>
+      <div class="col-md-8 kertas-biodata">
+      <div class="card">
+          <table class="table">
+            <tbody>
+              <tr>
+                <th scope="row"></th>
+                <th>Nama</th>
+                <td>:</td>
+                <td>Muh Rivaldo Setyo Purnomo</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>TTL</th>
+                <td>:</td>
+                <td>Nganjuk, 12 Maret 1999</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Jeni Kelamin</th>
+                <td>:</td>
+                <td>Laki-laki</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Alamat</th>
+                <td>:</td>
+                <td>Jl Ikan Gurame Lowokwaru Malang</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>No. Hp</th>
+                <td>:</td>
+                <td>085233423984</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Status</th>
+                <td>:</td>
+                <td>Mahasiswa</td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Hobi</th>
+                <td>:</td>
+                <td>Mancing</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Email</th>
+                <td>:</td>
+                <td>Rivalzsetyo@gmail.com</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Github</th>
+                <td>:</td>
+                <td>Rivaldz</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+   </div>
+  
+ </div>
+
+<footer class="footer">
+  <div class="container">
+   <p>Rivalzsetyo@gmail.com</p>
+  </div>
+</footer>
     </div>
   );
 }
