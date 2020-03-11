86a3bbaac71c5371d9e7e6e16fbb50745785e5fc
Rivaldz <rivalzsetyo@gmail.com>
2020-03-11 11:12:06 +0700

    menambahkan import dan nav

--- mrivaldo-minggu6_tugas/src/App.js
+++ mrivaldo-minggu6_tugas/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
-function App() {
+
+export default function Main() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class=" navbar navbar-expand-sm bg-primary navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Rivaldo Shop</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" href="/product">Lihat Product</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/profil">Profil</a>
+            </li>
+          </ul>
+        </nav>
     </div>
+</Router>
   );
 }
-
-export default App;
