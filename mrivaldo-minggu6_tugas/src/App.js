6cb78fe10c29269d19527dbdbe1260bf0e69d19f
Rivaldz <rivalzsetyo@gmail.com>
2020-03-13 19:03:28 +0700

    mengisi konten  sub menu eletronik

--- mrivaldo-minggu6_tugas/src/App.js
+++ mrivaldo-minggu6_tugas/src/App.js
@@ -11,10 +11,109 @@ import {
   Link
 } from "react-router-dom";
 
+export default function NestingExample(){
+  return(
+    <Router>
+      <div className="App">
+      <nav className=" navbar navbar-expand-sm bg-primary navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">Rivaldo Shop</a>
+          </li>
+          <li className="nav-item">
+            <Link to="topics"> 
+             <a className="nav-link">Lihat Product</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/profil">Profil</a>
+            </li>
+          </ul>
+        </nav>
+      {/* <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+        <hr/> */}
+        <Switch>
+          <Route exact path="/">
+              {/* <Topics /> */}
+              <Home/>
+          </Route>
 
-export default function Main() {
+          <Route path="/topics">
+              <Topics/>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Topics(){
+  let { path, url } = useRouteMatch();
+  return(
+    <div>
+      <h2>Daftar Kategori</h2>
+
+         <div> 
+          <Link to={`${url}/Elektronik`}>
+            <button type="button" className="btn btn-primary" > Elektronik  </button> 
+          </Link>
+
+          <Link to={`${url}/Fasion`}>
+            <button type="button" className="btn btn-danger" > Fasion  </button>
+          </Link>
+
+          <Link to={`${url}/Computer`}>
+             <button type="button" className="btn btn-info" > Computer  </button>
+          </Link>
+        </div>
+
+      <Switch>
+        <Route exact path={path}>
+        <h3> Please Select a Topics.</h3>
+        </Route>
+
+        <Route exact path={`${path}/:topicId`}>
+          <Topic/>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return(
+    <Link to={topicId}></Link>
+    <h5> {topicId} </h5>
+
+     <Route path="/Elektronik">
+     <Elektronik />
+     </Route>
+  );
+}
+
+function Elektronik(){
+  return(
+    <h5> INi ADALAH ELEKTRONIK</h5>
+  )
+}
+
+function Main() {
   return (
     <Router>
+      <h4>INI adalah Main</h4>
     <div className="App">
       <nav className=" navbar navbar-expand-sm bg-primary navbar-dark">
         <ul className="navbar-nav">
@@ -22,7 +121,9 @@ export default function Main() {
             <a className="nav-link" href="/">Rivaldo Shop</a>
           </li>
           <li className="nav-item">
-             <a className="nav-link" href="/product">Lihat Product</a>
+            <Link to="product"> 
+             <a className="nav-link">Lihat Product</a>
+            </Link>
           </li>
           <li className="nav-item">
               <a className="nav-link" href="/about">About</a>
@@ -33,18 +134,23 @@ export default function Main() {
           </ul>
         </nav>
         <Switch>
+
           <Route exact path="/">
             <Home />
           </Route>
+
           <Route path="/product">
             <Product />
           </Route>          
+
           <Route exact path="/about">
             <About/>
           </Route>          
+
           <Route exact path="/profil">
             <Profil/>
           </Route>
+
         </Switch>
     </div>
 </Router>
@@ -59,16 +165,19 @@ function Home(){
   );
 }
 function Product(){
-  let { path, url} = useRouteMatch();
+  let { path, url } = useRouteMatch();
   return (
     <div>
       <h2>INI ADALAH HALAMAN PRODUCT</h2>
       {/* <div class="container"> */}
-        <Link to={`${url}/Elektronik,Murah Meriah Bosss`}>
-        <button type="button" className="btn btn-primary" > Elektronik  </button>
+      <li>
+        <Link to={`${url}/Elektronik, Murah Meriah Bosss`}>
+        {/* <button type="button" className="btn btn-primary" > elektronik  </button> */}
         {/* Jancuocok */}
+        ini test 
          </Link>
       {/* </div> */}
+      </li>
 
       <button type="button" className="btn btn-danger" href={`${url}/makanan`}> Makanan</button>
       <button type="button" className="btn btn-info" href={`${url}/fasion`}> Fasion</button>
@@ -77,23 +186,24 @@ function Product(){
         <Route exact path={path}>
             <h3> Please Pilih Katagori.</h3>
         </Route>
-        <Router path={`${path}/:kategori`}>
+
+        <Router exact path={`${path}/:produkId`}>
           <Produk />
         </Router>
+
       </Switch>
     </div>
   );
 }
 
 function Produk(){
-  let { kategori } = useParams();
+  let { produkId } = useParams();
   return(
     <div>
-      <h3>
-        { kategori }
-      </h3>
+      <h1>
+        {produkId}
+      </h1>
     </div>
-
   );
 }
 
