456aaa9684e5cb80ea3a742d88c782f9efec5ee7
Rivaldz <rivalzsetyo@gmail.com>
2020-04-08 16:02:31 +0700

    modify Delete.js and Post.js

---
+++ mrivaldo-minggu10/src/Services/Post.js
@@ -0,0 +1,22 @@
+import {domainPath} from "./config";
+
+const PostAPI = (path, data) => {
+    const promise = new Promise((resolve, reject) => {
+        fetch(`${domainPath}/${path}`, {
+            method: 'post',
+            header: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((result: Response) => {
+                resolve(result);
+            },(err) => {
+                reject(err);
+            })
+    })
+    return promise;
+} 
+
+export default PostAPI;
