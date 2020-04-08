8f0faf26ee3c755d4c436cc688f0741abc594f62
Rivaldz <rivalzsetyo@gmail.com>
2020-04-08 15:51:46 +0700

    selesai praktikum 3

--- mrivaldo-minggu10/src/Services/index.js
+++ mrivaldo-minggu10/src/Services/index.js
@@ -12,9 +12,43 @@ const GetApi = (path) =>{
     return promise;
 }
 
+const PostAPI = (path, data) => {
+    const promise = new Promise((resolve, reject)=>{
+        fetch(`${ domainPath }/${path}`,{
+            method: 'post',
+            header: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((result: Response) => {
+                resolve(result);
+            }, (err) => {
+                reject(err);
+            })
+    })
+    return promise;
+}
+
+const DeleteAPI = (path, data) => {
+    const promise = new Promise((resolve, reject ) => {
+        fetch(`${domainPath}/${path}/${data}`,{method: 'DELETE'})
+            .then((result: Response) => {
+                resolve(result);
+            }, (err) => {
+                reject(err);
+            })
+    })
+}
+
 const getNewsBlog = () => GetApi('posts?_sort=id&_order=desc')
+const postNewsBlog = (dataYgDikirim) => PostAPI('posts', dataYgDikirim);
+const deleteNewsBlog = (dataYgDiHapus) => DeleteAPI('post', dataYgDiHapus);
 const API = {
-    getNewsBlog
+    getNewsBlog,
+    postNewsBlog,
+    dataYgDiHapus
 }
 
 export default API;
