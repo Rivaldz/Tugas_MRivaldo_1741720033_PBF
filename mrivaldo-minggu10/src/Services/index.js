b11276c3bef5852dee6b48be565a8e59c04dd564
Rivaldz <rivalzsetyo@gmail.com>
2020-04-08 16:04:11 +0700

    modify index.js

--- mrivaldo-minggu10/src/Services/index.js
+++ mrivaldo-minggu10/src/Services/index.js
@@ -1,46 +1,50 @@
-const domainPath = 'http://localhost:3001';
-const GetApi = (path) =>{
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}`)
-            .then(response => response.json())
-            .then((result) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
+import GetApi from "./Get"; 
+import PostAPI from "./Post";
+import DeleteAPI from "./Delete";
 
-const PostAPI = (path, data) => {
-    const promise = new Promise((resolve, reject)=>{
-        fetch(`${ domainPath }/${path}`,{
-            method: 'post',
-            header: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then((result: Response) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
+// const domainPath = 'http://localhost:3001';
+// const GetApi = (path) =>{
+//     const promise = new Promise((resolve, reject) => {
+//         fetch(`${domainPath}/${path}`)
+//             .then(response => response.json())
+//             .then((result) => {
+//                 resolve(result);
+//             }, (err) => {
+//                 reject(err);
+//             })
+//     })
+//     return promise;
+// }
 
-const DeleteAPI = (path, data) => {
-    const promise = new Promise((resolve, reject ) => {
-        fetch(`${domainPath}/${path}/${data}`,{method: 'DELETE'})
-            .then((result: Response) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-}
+// const PostAPI = (path, data) => {
+//     const promise = new Promise((resolve, reject)=>{
+//         fetch(`${ domainPath }/${path}`,{
+//             method: 'post',
+//             header: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         })
+//             .then((result: Response) => {
+//                 resolve(result);
+//             }, (err) => {
+//                 reject(err);
+//             })
+//     })
+//     return promise;
+// }
+
+// const DeleteAPI = (path, data) => {
+//     const promise = new Promise((resolve, reject ) => {
+//         fetch(`${domainPath}/${path}/${data}`,{method: 'DELETE'})
+//             .then((result: Response) => {
+//                 resolve(result);
+//             }, (err) => {
+//                 reject(err);
+//             })
+//     })
+// }
 
 const getNewsBlog = () => GetApi('posts?_sort=id&_order=desc')
 const postNewsBlog = (dataYgDikirim) => PostAPI('posts', dataYgDikirim);
