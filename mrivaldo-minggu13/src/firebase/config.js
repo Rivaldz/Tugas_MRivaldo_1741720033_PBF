9a074b09b01fcd2b0f4432853bdd37658e89e0be
Rivaldz <rivalzsetyo@gmail.com>
2020-04-29 21:54:11 +0700

    meribah blogpost.jsx

---
+++ mrivaldo-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB71ht3TRmD3pgZsHVIeakoW9asU7S1XeQ",
+  authDomain: "fir-c3638.firebaseapp.com",
+  databaseURL: "https://fir-c3638.firebaseio.com",
+  projectId: "fir-c3638",
+  storageBucket: "fir-c3638.appspot.com",
+  messagingSenderId: "10373462604",
+  appId: "1:10373462604:web:dda3eb3f4d27af8a01d2e9",
+  measurementId: "G-B8TQ3TL5B7"
+};
+
+export default firebaseConfig;
